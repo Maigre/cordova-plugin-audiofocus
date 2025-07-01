c955aa9af1bdc758fb1424fd3c359d8368271a93
maigre <thomas@37m.gr>
2025-07-01 19:18:47 +0200

    cancelFocus and onFocusChange callback

--- src/android/AudioFocus.java
+++ src/android/AudioFocus.java
@@ -5,21 +5,58 @@ import android.media.AudioManager;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class AudioFocus extends CordovaPlugin {
+    private CallbackContext focusChangeCallbackContext;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("requestFocus")) {
             this.requestFocus(callbackContext);
             return true;
         }
-
+        if (action.equals("cancelFocus")) {
+            this.cancelFocus(callbackContext);
+            return true;
+        }
+        if (action.equals("onFocusChange")) {
+            this.onFocusChange(callbackContext);
+            return true;
+        }
         return false;
     }
+
+    private AudioManager.OnAudioFocusChangeListener listener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChangeCallbackContext != null) {
+                String focusState;
+                switch (focusChange) {
+                    case AudioManager.AUDIOFOCUS_GAIN:
+                        focusState = "AUDIOFOCUS_GAIN";
+                        break;
+                    case AudioManager.AUDIOFOCUS_LOSS:
+                        focusState = "AUDIOFOCUS_LOSS";
+                        break;
+                    case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                        focusState = "AUDIOFOCUS_LOSS_TRANSIENT";
+                        break;
+                    case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                        focusState = "AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK";
+                        break;
+                    default:
+                        focusState = "AUDIOFOCUS_UNKNOWN";
+                }
+                PluginResult result = new PluginResult(PluginResult.Status.OK, focusState);
+                result.setKeepCallback(true);
+                focusChangeCallbackContext.sendPluginResult(result);
+            }
+        }
+    };
 
     private void requestFocus(CallbackContext callbackContext) {
         // get AudioManager
@@ -28,7 +65,7 @@ public class AudioFocus extends CordovaPlugin {
                                     .getSystemService(Context.AUDIO_SERVICE);
 
         // request audio focus
-        int result = am.requestAudioFocus(null,
+        int result = am.requestAudioFocus(listener,
                                         AudioManager.STREAM_MUSIC,
                                         AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
 
@@ -38,5 +75,30 @@ public class AudioFocus extends CordovaPlugin {
         } else {
             callbackContext.error("");
         }
+    }
+
+    private void cancelFocus(CallbackContext callbackContext) {
+
+         // get AudioManager
+        AudioManager am = (AudioManager) this.cordova.getActivity()
+                                    .getApplicationContext()
+                                    .getSystemService(Context.AUDIO_SERVICE);
+
+        int result = am.abandonAudioFocus(listener);
+
+        // return result
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            callbackContext.success("");
+        } else {
+            callbackContext.error("");
+        }
+    }
+
+    private void onFocusChange(CallbackContext callbackContext) {
+        this.focusChangeCallbackContext = callbackContext;
+        // Keep callback alive for future events
+        PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        result.setKeepCallback(true);
+        callbackContext.sendPluginResult(result);
     }
 }
--- www/AudioFocus.js
+++ www/AudioFocus.js
@@ -1,8 +1,19 @@
 var exec = require('cordova/exec');
 
 exports.requestFocus = function(success, error) {
-    success = success || function() { };
-    error = error || function() { };
+    success = success || function() {};
+    error = error || function() {};
 
     exec(success, error, "AudioFocus", "requestFocus", []);
+};
+
+exports.cancelFocus = function (success, error) {
+  success = success || function () {};
+  error = error || function () {};
+
+  exec(success, error, 'AudioFocus', 'cancelFocus', []);
+};
+
+exports.onFocusChange = function(callback) {
+    exec(callback, null, 'AudioFocus', 'onFocusChange', []);
 };
